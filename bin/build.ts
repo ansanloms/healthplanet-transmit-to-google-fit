db96105e4723812b9e85de9e89555f2d18ad2dfb
ansanloms <ansanloms@users.noreply.github.com>
2024-03-01 14:52:20 +0900

    upgrade

--- bin/build.ts
+++ bin/build.ts
@@ -30,7 +30,6 @@ const copy = async (filename: string) => {
 await build({
   entryPoints: [path.join(__dirname, "../src/main.ts")],
   bundle: true,
-  minify: true,
   outfile: path.join(__dirname, "../dist/bundle.js"),
   plugins: [httpFetch, GasPlugin],
 });
